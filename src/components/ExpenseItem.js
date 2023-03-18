2088531e68352a14894fa4f696b23486cd87388e
Ebrahim <ebrahim.mozafary@gmail.com>
2023-03-18 17:21:04 +0330

    section 15

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,20 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./expense/ExpenseDate";
+import Card from "./expense/Card";
 function ExpenseItem(props) {
     // const expenseDate = new Date(2021, 2 ,28);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.55;
 
     return (
-        <div className="ex-item">
+        <Card className="ex-item">
             <ExpenseDate  date={props.date} />
             <div>
                 {props.title}
             </div>
                 <div className="ex-item1__price">${props.amount}</div>
 
-        </div>
+        </Card>
     )
 }
 
