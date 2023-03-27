961daa1ce6dd3208986a89a90cd36866170842e1
Ebrahim <ebrahim.mozafary@gmail.com>
2023-03-27 15:58:46 +0330

    state management start

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
         {id: 'dd', title: 'car insurance', amount: 255.2, date: new Date(2021, 5, 12)},
         {id: 'ef', title: 'carwash', amount: 156.3, date: new Date(2021, 6, 24)},
         {id: 'fg', title: 'car tire', amount: 485.6, date: new Date(2021, 1, 10)},
-        {id: 'gh', title: 'car insurance', amount: 752.9, date: new Date(2021, 12, 1)},
+        {id: 'gh', title: 'car ghazgholang', amount: 752.9, date: new Date(2021, 12, 1)},
     ]
 
     return (
@@ -18,12 +18,14 @@ const App = () => {
 
             {/*</header>*/}
             <Card className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+                {/*<img src={logo} className="App-logo" alt="logo"/>*/}
                 <p className="myFont">تست فونت</p>
                 <p className="kalameh_black">تست فونت</p>
                 <Card className="mainCard">
                     <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-                    <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+                    <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+                    <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+                    <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
                 </Card>
 
                 <p>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,29 @@
+import React, {useState} from "react";
 import './ExpenseItem.css'
 import ExpenseDate from "./expense/ExpenseDate";
 import Card from "./expense/Card";
 function ExpenseItem(props) {
+    const [expenseTitle,setTitle] = useState(props.title);
+    console.log('expense item evaluate')
     // const expenseDate = new Date(2021, 2 ,28);
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.55;
+     //let expenseTitle = props.title;
+
+    const changeTitle = () => {
+        setTitle('updated');
+        console.log("hi",expenseTitle)
+        //expenseTitle = 'change titless'
+    }
 
     return (
         <Card className="ex-item">
             <ExpenseDate  date={props.date} />
             <div>
-                {props.title}
+                {expenseTitle}
             </div>
                 <div className="ex-item1__price">${props.amount}</div>
+            <button onClick={changeTitle}>Change Title</button>
 
         </Card>
     )
